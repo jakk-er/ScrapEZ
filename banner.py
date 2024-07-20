de9fcd607d4fe068e19daa3a045bff8f39098f3a
jakk-er <jakkbaloch@gmail.com>
2024-07-20 00:44:25 -0400

    Update banner.py

--- banner.py
+++ banner.py
@@ -2,18 +2,23 @@
 # You must give appropriate credit, provide a link to the license, and indicate if changes were made.
 # Details: https://creativecommons.org/licenses/by/4.0/
 
+banner_displayed = False
+
 def display_banner():
-      banner = """
-      ===================================================
-      ====  __                             __  _____ ====
-      ==== / _\  ___  _ __  __ _  _ __    /__\/ _  / ====
-      ==== \ \  / __|| '__|/ _` || '_ \  /_\  \// /  ====
-      ==== _\ \| (__ | |  | (_| || |_) |//__   / //\ ====
-      ==== \__/ \___||_|   \__,_|| .__/ \__/  /____/ ====
-      ==== ScrapEZ webscraper    |_|      by jakk-er ====
-      ===================================================
-            \n \n"""
-      print(banner)
-      
+    global banner_displayed
+    if not banner_displayed:
+        banner = """
+        ===================================================
+        ====  __                             __  _____ ====
+        ==== / _\  ___  _ __  __ _  _ __    /__\/ _  / ====
+        ==== \ \  / __|| '__|/ _` || '_ \  /_\  \// /  ====
+        ==== _\ \| (__ | |  | (_| || |_) |//__   / //\ ====
+        ==== \__/ \___||_|   \__,_|| .__/ \__/  /____/ ====
+        ==== ScrapEZ webscraper    |_|      by jakk-er ====
+        ===================================================
+              \n \n"""
+        print(banner)
+        banner_displayed = True
+
 if __name__ == "__main__":
     display_banner()
